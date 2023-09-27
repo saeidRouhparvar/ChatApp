c02b1854ab24e67e929a3a5c70ca6d43c326335c
saeid <saeid.rohparvar@gmail.com>
2023-09-27 22:58:35 +0330

    create avatar

---
+++ src/component/other/avatar/Avatar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './avatar.css'
+
+ const Avatar = () => {
+  return (
+    <div className='avatarBorder'>
+        <div className="avatar">
+          <img src="" />
+        </div>
+    </div>
+  )
+}
+export default Avatar 
